6b261d1b3ceba003e7b53e90c9b7f3475d2a0bfe
rodrigomagano <rcarvalhomagano@gmail.com>
2025-07-05 20:01:54 -0300

    comite do dia 05/07/2025

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,16 +15,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		var consumoapi = new ConsumoApi();
-		var json = 	consumoapi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=7c9249f7");
-
-		System.out.println(json);
-
-		var conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterdados(json, DadosSerie.class);
-		System.out.println();
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
 	}
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,4 +1,56 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Principal {
+
+    private  Scanner leitura = new Scanner(System.in);
+
+    private ConsumoApi consumoapi = new ConsumoApi();
+
+   private ConverteDados conversor = new ConverteDados();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String APIKEY = "&apikey=7c9249f7";
+
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série:");
+        var nomeSerie = leitura.nextLine();
+
+        var json = consumoapi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + APIKEY);
+
+        DadosSerie dados = conversor.obterdados(json, DadosSerie.class);
+
+        if (dados.titulo() == null || dados.titulo().equalsIgnoreCase("N/A")) {
+            System.out.println("Série não encontrada. Verifique o nome digitado.");
+            return;
+        }
+
+        System.out.println(dados); // Exibe os dados recebidos
+
+        Integer temporadas = dados.totalTemporadas();
+
+        if (temporadas == null) {
+            System.out.println("Número de temporadas não disponível.");
+            return;
+        }
+
+        List<DadosTemporada> listaTemporadas = new ArrayList<>();
+
+        for (int i = 1; i <= temporadas; i++) {
+            json = consumoapi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + APIKEY);
+            DadosTemporada dadosTemporada = conversor.obterdados(json, DadosTemporada.class);
+            listaTemporadas.add(dadosTemporada);
+        }
+
+        listaTemporadas.forEach(System.out::println);
+        listaTemporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+    }
 }
